c1604b710213e44b946e9c2c357c450f5bdbbf79
AlexEmlnv <alex17prog@gmail.com>
2022-06-30 10:16:18 +0300

    Clean up MAIN branch

--- src/dto/PageDTO.ts
+++
@@ -1,9 +0,0 @@
-export class PageDTO {
-	size : bigint;
-	index : bigint;
-
-	constructor(_size: bigint, _index: bigint) {
-		this.size = _size;
-		this.index = _index;
-	} 
-}
--- src/dto/SortDTO.ts
+++
@@ -1,9 +0,0 @@
-export class SortDTO {
-	name : string;
-	order : string;
-
-	constructor(_name: string, _order: string) {
-		this.name = _name;
-		this.order = _order;
-	} 
-}
